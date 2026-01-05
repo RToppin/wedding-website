e2b9d427d99c0c39587ea52a4f7bfa2658a50166
RToppin <146687823+RToppin@users.noreply.github.com>
2026-01-04 19:56:45 -0500

    Last Working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
     <>
-        <div className="font-playfair h-96 p-8 bg-emerald-900 text-gold-500 text-center">
+        <div className="font-playfair h-96 p-8 text-gold-500 text-center">
             Website Created by Ryan Toppin
         </div>
     </>
---
+++ src/pages/FAQ.jsx
@@ -0,0 +1,7 @@
+function FAQ (){
+    return(
+        <div className="text-center text-4xl py-8 text-emerald-950 font-playfair justify-center min-h-screen max-h-fit bg-emerald-50">FAQ</div>
+    );
+}
+
+export default FAQ
